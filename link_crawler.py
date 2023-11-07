30b0bed5d16e38c0bc96fb99d52d39d6260d6f39
Ali <46873759+Ghoreish@users.noreply.github.com>
2023-11-07 15:21:03 +0330

    adding some tools
    link_crawler and link_list_grabber.
    these are not user friendly but they can help you.
    use them in recon phase or other phases

---
+++ link_crawler.py
@@ -0,0 +1,70 @@
+import re
+import requests
+
+def getlinks(text):
+    links = re.findall('[\'\"](http[s]?://[^\'\"<>\n]+)', text)
+    return links
+
+
+def getpaths(text):
+    paths = re.findall('[\'\"](/[-a-zA-Z0-9()!@:%_+.~#?&=]+[-a-zA-Z0-9()!@:%_+.~#?&/=]+)[\'\"]', text)
+    if '//' in paths:
+        print(text)
+    return paths
+
+
+def gethost(link):
+    hostname = re.findall('(http[s]?://)?([a-zA-Z0-9.]+[.][a-zA-Z]+)', link)
+    try:
+        return hostname[0][0] + hostname[0][1]
+    except:
+        return []
+
+
+def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy=None):
+    if link in checked_links:
+        return None
+    checked_links.append(link)
+    hostname = gethost(link)
+    if allowed_host not in hostname:
+        return None
+    try:
+        r = requests.get(link, verify=False, proxies=proxy)
+        res = r.text
+    except:
+        return None
+    if r.url not in selected_links:
+        selected_links.append(r.url)
+    pure_links = getlinks(res)
+    pure_paths = getpaths(res)
+    links = []
+    for i in pure_links:
+        if i not in checked_links:
+            links.append(i)
+    for i in pure_paths:
+        temp_link = hostname + i
+        if temp_link not in checked_links:
+            links.append(temp_link)
+    for i in links:
+        if i not in checked_links:
+            extract_links(i, allowed_host, checked_links)
+    return selected_links
+
+
+
+proxy = {'http': 'http://127.0.0.1:8080',
+         'https': 'http://127.0.0.1:8080'}
+
+l = extract_links('{example link}', '{example host}', proxy=proxy)
+
+
+f = open('boss.txt', 'w')
+for i in l:
+    try:
+        f.write(i)
+        f.write('\n')
+    except:
+        print(i)
+f.close()
+for i in l:
+    print(i)
---
+++ link_list_grabber.py
@@ -0,0 +1,36 @@
+import os
+import requests
+import time
+
+def grab_from_list(dirname: str, links: list, proxy=None):
+    os.mkdir(dirname)
+    for i in links:
+        if i[-1] == '/':
+            i = i[:-1]
+        filename = i.split('/')[-1]
+        if '.' not in filename:
+            filename += '.html'
+        path = dirname + '/' + filename
+        try:
+            r = requests.get(i, verify=False, proxies=proxy)
+        except:
+            print(i)
+            continue
+        try:
+            f = open(path, 'wb')
+        except:
+            f = open('files/' + str(time.time()) + '.html', 'wb')
+        f.write(b'//')
+        f.write(r.url.encode())
+        f.write(b'\n//')
+        f.write(str(r.headers).encode())
+        f.write(b'\n')
+        f.write(r.content)
+        f.close()
+
+f = open('boss.txt', 'r')
+res = f.readlines()
+res = [x[:-1] for x in res]
+proxy = {'http': 'http://127.0.0.1:8080',
+         'https': 'http://127.0.0.1:8080'}
+grab_from_list('files', res, proxy=proxy)
