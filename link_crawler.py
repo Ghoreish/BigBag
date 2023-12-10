513ac9749bb92ad4f4a670fd0ea3fc72de121cf4
Ali <46873759+Ghoreish@users.noreply.github.com>
2023-12-10 15:38:51 +0330

    Update link_crawler.py
    doesn't check the binary files

--- link_crawler.py
+++ link_crawler.py
@@ -2,7 +2,7 @@ import re
 import requests
 
 def getlinks(text):
-    links = re.findall('[\'\"](http[s]?://[^\'\"<>\n]+)', text)
+    links = re.findall('[\'\">](http[s]?://[^\'\"<>\n]+)', text)
     return links
 
 
@@ -20,19 +20,45 @@ def gethost(link):
     except:
         return []
 
+def check_for_large(url):
+    exts = [".avi", ".flv", ".avchd", ".jpeg", ".png", ".gif", ".tiff", ".bmp", ".eps", ".mp3", ".mp4", ".wav", ".flac", ".aiff", ".jpg", ".pdf"]
+    url = url.lower()
+    for i in exts:
+        if i in url:
+            return True
+    return False
 
-def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy=None):
+def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy=None, cookies={}):
+    print(link)
+    if check_for_large(link):
+        return None
+    if link in checked_links:
+        return None
+    hostname = gethost(link)
+    if allowed_host not in hostname:
+        return None
+    try:
+        r = requests.get(link, allow_redirects=True, proxies=proxy, verify=False)
+        res = r.text
+    except Exception as e:
+        print(link)
+        print(e)
+        return None
     if link in checked_links:
         return None
     checked_links.append(link)
     hostname = gethost(link)
     if allowed_host not in hostname:
         return None
-    try:
-        r = requests.get(link, verify=False, proxies=proxy)
-        res = r.text
-    except:
-        return None
+    f = open('links.txt', 'w')
+    for i in selected_links:
+        try:
+            f.write(i)
+            f.write('\n')
+        except Exception as e:
+            print(e)
+            print(i)
+    f.close()
     if r.url not in selected_links:
         selected_links.append(r.url)
     pure_links = getlinks(res)
@@ -47,24 +73,12 @@ def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy
             links.append(temp_link)
     for i in links:
         if i not in checked_links:
-            extract_links(i, allowed_host, checked_links)
+            extract_links(i, allowed_host, checked_links, proxy=proxy, cookies=cookies)
     return selected_links
 
 
 
 proxy = {'http': 'http://127.0.0.1:8080',
          'https': 'http://127.0.0.1:8080'}
-
-l = extract_links('{example link}', '{example host}', proxy=proxy)
-
-
-f = open('links.txt', 'w')
-for i in l:
-    try:
-        f.write(i)
-        f.write('\n')
-    except:
-        print(i)
-f.close()
-for i in l:
-    print(i)
+cookie = {}
+l = extract_links('https://example.com', 'https://example.com', proxy=proxy, cookies=cookie)
