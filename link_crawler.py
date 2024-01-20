495e2be0f17ad8ce180e08840ae10c4e69b724ff
Ali <46873759+Ghoreish@users.noreply.github.com>
2024-01-20 09:58:31 +0330

    Update link_crawler.py
    better real-time output

--- link_crawler.py
+++ link_crawler.py
@@ -1,13 +1,14 @@
 import re
 import requests
 
+
 def getlinks(text):
     links = re.findall('[\'\">](http[s]?://[^\'\"<>\n]+)', text)
     return links
 
 
 def getpaths(text):
-    paths = re.findall('[\'\"](/[-a-zA-Z0-9()!@:%_+.~#?&=]+[-a-zA-Z0-9()!@:%_+.~#?&/=]+)[\'\"]', text)
+    paths = re.findall('[\'\"]([./]*[/][-a-zA-Z0-9()!@:%_+.~#?&=]+[-a-zA-Z0-9()!@:%_+.~#?&/=]+)[\'\"]', text)
     if '//' in paths:
         print(text)
     return paths
@@ -20,25 +21,29 @@ def gethost(link):
     except:
         return []
 
-def check_for_large(url):
-    exts = [".avi", ".flv", ".avchd", ".jpeg", ".png", ".gif", ".tiff", ".bmp", ".eps", ".mp3", ".mp4", ".wav", ".flac", ".aiff", ".jpg", ".pdf"]
-    url = url.lower()
-    for i in exts:
-        if i in url:
-            return True
-    return False
 
-def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy=None, cookies={}):
+def check_for_large(url, proxy):
+    r = requests.head(url, verify=False, proxies=proxy)
+    if 'Content-Length' not in r.headers:
+        return False
+    cl = r.headers['Content-Length']
+    if int(cl) > 2000000:
+        return True
+    else:
+        return False
+
+
+def extract_links(link, allowed_host, filename, checked_links=[], selected_links=[], proxy=None, cookies={}):
     print(link)
-    if check_for_large(link):
-        return None
     if link in checked_links:
         return None
     hostname = gethost(link)
     if allowed_host not in hostname:
         return None
+    if check_for_large(link, proxy):
+        return None
     try:
-        r = requests.get(link, allow_redirects=True, proxies=proxy, verify=False)
+        r = requests.get(link, allow_redirects=True, proxies=proxy, verify=False, cookies=cookies)
         res = r.text
     except Exception as e:
         print(link)
@@ -50,7 +55,7 @@ def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy
     hostname = gethost(link)
     if allowed_host not in hostname:
         return None
-    f = open('links.txt', 'w')
+    f = open(filename, 'w')
     for i in selected_links:
         try:
             f.write(i)
@@ -68,17 +73,35 @@ def extract_links(link, allowed_host, checked_links=[], selected_links=[], proxy
         if i not in checked_links:
             links.append(i)
     for i in pure_paths:
-        temp_link = hostname + i
+        if i[0] != '/':
+            temp_link = link + '/' + i
+        else:
+            temp_link = hostname + i
+        print(temp_link, 1)
+
         if temp_link not in checked_links:
             links.append(temp_link)
     for i in links:
         if i not in checked_links:
-            extract_links(i, allowed_host, checked_links, proxy=proxy, cookies=cookies)
+            extract_links(i, allowed_host, filename, checked_links, proxy=proxy, cookies=cookies)
     return selected_links
 
 
-
+filename = 'example.txt'
+url = 'https://example.com'
+allowed_host = 'https://example.com'
 proxy = {'http': 'http://127.0.0.1:8080',
          'https': 'http://127.0.0.1:8080'}
 cookie = {}
-l = extract_links('https://example.com', 'https://example.com', proxy=proxy, cookies=cookie)
+l = extract_links(url, allowed_host, filename, proxy=proxy, cookies=cookie)
+
+f = open(filename, 'w')
+for i in l:
+    try:
+        f.write(i)
+        f.write('\n')
+    except:
+        print(i)
+f.close()
+for i in l:
+    print(i)
